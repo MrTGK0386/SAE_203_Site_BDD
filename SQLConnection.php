f31aaacff7d5d1b9353636347a712d58c2581116
ZyFou <zyfodexe@gmail.com>
2023-06-19 13:47:13 +0200

    added sql connection file and edited createUserTable

---
+++ SQLConnection.php
@@ -0,0 +1,19 @@
+<?php
+
+$servername = "localhost:13306";
+$username = "root";
+$password = "";
+$dbname = "gaetan.bondenet";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check the connection
+if (!$conn) {
+    die("La connexion a échoué : " . mysqli_connect_error());
+}
+else{
+    // echo "connection reussie";
+};
+
+?>
