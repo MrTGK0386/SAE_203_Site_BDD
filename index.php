c25d474ecaca1972ae930b907cdfa871e7f011af
mrtgk <mrtiti30@gmail.com>
2023-06-20 09:16:32 +0200

    Ajout database via LOAD DATA INFILE Index de zyfou Test 5

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>La page de fou - Page d'accueil</title>
     <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
     >
     <?php
     include_once 'sql_usage/SQLconnection.php';
