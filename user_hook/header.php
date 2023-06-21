ff625a8f8c0f13044059563cac1c6c0e106c1c91
mrtgk <mrtiti30@gmail.com>
2023-06-21 15:50:43 +0200

    modif header.php

--- user_hook/header.php
+++ user_hook/header.php
@@ -1,23 +1,14 @@
 <?php
+if (isset($_SESSION['username'])) {
+    if ($_SESSION['admin'] == 1){
+        include_once "HTML_elements/headers/headerAdmin.html";
+    }
+    else{
+        include_once "HTML_elements/headers/headerUser.html";
 
-function checkAdmin(){
-    if (isset($_SESSION['admin'])){
-        if ($_SESSION['admin'] = 1){
-            include_once "HTML_elements/headers/headerAdmin.html";
-        }
-        else {
-            include_once "HTML_elements/headers/headerUser.html";
-        }
     }
-    else {
-        include_once "HTML_elements/headers/headerRandom.html";
-    }
+    include_once "HTML_elements/headers/headerRandom.html";
 }
-
-
-
-
-checkAdmin();
 
 ?>
 <script> var session = <?php echo $_SESSION['admin']; ?>; alert(session);</script>
