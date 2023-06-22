f29d66c9772a90e799edcffb225ac2938d52b4c7
mrtgk <mrtiti30@gmail.com>
2023-06-22 12:08:10 +0200

    Ajout bouton dans le index
    ça marche pas encore sur les headers

--- Scripts/main.js
+++ Scripts/main.js
@@ -4,15 +4,38 @@ function init (){
 
 function themeSwitch () {
     let button = document.getElementById('btnSwitch');
+    let dad = document.documentElement;
+    let state ;
+    window.addEventListener('load', () =>{
+        if (dad.getAttribute('data-bs-theme') != state){
+            if (state == 'light') {
+                state = dad.getAttribute('data-bs-theme');
+                dad.setAttribute('data-bs-theme','light');
+                button.classList.add("btn-secondary");
+                button.classList.remove("btn-light");
+                document.getElementById("icoSwtch").setAttribute('icon','ph:sun-bold');
+            }
+            else {
+                state = dad.getAttribute('data-bs-theme');
+                dad.setAttribute('data-bs-theme','dark');
+                button.classList.add("btn-light");
+                button.classList.remove("btn-secondary");
+                document.getElementById("icoSwtch").setAttribute('icon','ph:moon-bold');
+            }
+        }
+    })
+
     button.addEventListener('click',()=>{
-        if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-            document.documentElement.setAttribute('data-bs-theme','light');
+        if (dad.getAttribute('data-bs-theme') == 'dark') {
+            state = dad.getAttribute('data-bs-theme');
+            dad.setAttribute('data-bs-theme','light');
             button.classList.add("btn-secondary");
             button.classList.remove("btn-light");
             document.getElementById("icoSwtch").setAttribute('icon','ph:sun-bold');
         }
         else {
-            document.documentElement.setAttribute('data-bs-theme','dark');
+            state = dad.getAttribute('data-bs-theme');
+            dad.setAttribute('data-bs-theme','dark');
             button.classList.add("btn-light");
             button.classList.remove("btn-secondary");
             document.getElementById("icoSwtch").setAttribute('icon','ph:moon-bold');
--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <link href="https://cesium.com/downloads/cesiumjs/releases/1.106/Build/Cesium/Widgets/widgets.css" rel="stylesheet">
     <?php
     session_start();
-    include_once 'sql_usage/createTables.php'
+    include_once 'sql_usage/createTables.php';
     ?>
 </head>
 
@@ -107,7 +107,7 @@
 </div>
 
 <!-- inclusion du bas de page du site -->
-<?php include_once('HTML_elements/footer.php'); ?>
+<?php include_once'HTML_elements/footer.php'; include_once "HTML_elements/ligthSwitch.php"; ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 <script>viewer.zoomTo(viewer.entities);</script>
 <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
