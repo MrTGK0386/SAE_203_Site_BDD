2ea79eaa07df1bffcbe88239c6e6ad9edd5c6966
Juuhey <jujupantel@gmail.com>
2023-06-23 19:28:05 +0200

    ALED C BUGGÉ

--- filters/meteor.php
+++ filters/meteor.php
@@ -1,163 +1,54 @@
 <?php
 
-// BOUTON APPLICATION DES FILTRES
+    $reqM = "SELECT GeoLocation FROM sae203_meteor WHERE `year` > ? AND `mass_(g)` > ? LIMIT ?";
+        if (isset($_POST["totalM"])){
+            $charge = $_POST["totalM"];
+        }
 
-if (isset($_POST["filter"])) {
+        if (isset($_POST["mYear"])){
+            $yearM = $_POST["mYear"];
+        } else{
+            $yearM = 860;
+        }
 
-    // TEST DES FILTRES QUI VONT ÊTRE APPLIQUÉS, SI PAS CHOISI ALORS VALEUR PAR DÉFAUT
+        if (isset($_POST["mass"])){
+            $massM = $_POST["mass"];
+        } else{
+            $massM = 0;
+        }
 
-    // DERNIERE ERUPTION EN DATE
-    if (isset($_POST["EruptionYear"])) {
-        $lastEruptionYear = $_POST["EruptionYear"];
-    } else{
-        $lastEruptionYear = -10500;
-    }
+        echo "$charge\n";
+        echo "$yearM\n";
+        echo "$massM\n";
 
-    // ALTITUDE MINIMALE
-    if (isset($_POST["elevation"])) {
-        $altitude = $_POST["elevation"];
-    } else{
-        $altitude = 0;
-    }
+        $stmtM = mysqli_prepare($conn, $reqM);
+        mysqli_stmt_bind_param($stmtM, 'sib', $charge, $yearM, $massM);
+        mysqli_stmt_execute($stmtM);
+        $resultM = mysqli_stmt_get_result($stmtM);
 
-    // AFFICHAGE DES VOLCANS SOUS-MARINS 
-    if (isset($_POST["underwater"])) {
-        $udw = 0;
+        var_dump($resultM); //le résultat est vide
 
-        $query2 = "SELECT latitude, longitude FROM SAE203_volcano WHERE elevation < ? AND last_eruption_year>= ?";
-        $statement2 = mysqli_prepare($conn, $query2);
-        mysqli_stmt_bind_param($statement2, 'ii', $udw, $lastEruptionYear);
-        mysqli_stmt_execute($statement2);
-        $result2 = mysqli_stmt_get_result($statement2);
-        
-        if (!$result2) {
+        if (!$resultM) {
             die("Erreur lors de l'exécution de la requête: " . mysqli_error($conn));
         }
 
-        $aled = 0;
-        while ($row2 = mysqli_fetch_assoc($result2)) {
-            echo "<script>addPoint($row2[latitude], $row2[longitude], 10, Cesium.Color.BLUE);</script>";
-            //$aled += 1;
+        echo"ALED";
+        while ($rowM = mysqli_fetch_assoc($resultM)) {
+            echo"ALED MILIEU";
+            $cleanString = str_replace(array('(', ')', ' '), '', $rowM['GeoLocation']);
+            
+            echo"ALED V2";
+            // Explode the string using the comma as the delimiter
+            $values = explode(',', $cleanString);
+            print_r($values);
+
+            // Retrieve the first and second values
+            $latitude = $values[0];
+
+            $longitude = $values[1];
+
+            echo"$longitude";
+            echo"<script>addPoint('$latitude', '$longitude', 4, Cesium.Color.GOLD);</script>";
         }
-        //echo "udw $aled";
-    }
-
-    // REQUÊTE POUR L'APPLICATION DES FILTRES
-
-    $queryBig = "SELECT latitude, longitude FROM SAE203_volcano WHERE last_eruption_year >= ? AND elevation >= ?";
-
-    // VÉRIFIE SI UN PAYS À ÉTÉ SÉLECTIONNÉ POUR L'AJOUTER À LA REQUÊTE
-    if (isset($_POST["country"]) && $_POST["country"] !== "Tous" && $_POST["country"] !== "") {
-        $pays = $_POST["country"];
-        $queryBig .= " AND country = ?";
-        $statementBig = mysqli_prepare($conn, $queryBig);
-        mysqli_stmt_bind_param($statementBig, 'iis', $lastEruptionYear, $altitude, $pays);
-        echo"$queryBig";
-    } else {
-        $statementBig = mysqli_prepare($conn, $queryBig);
-        mysqli_stmt_bind_param($statementBig, 'ii', $lastEruptionYear, $altitude);
-    }
-
-    mysqli_stmt_execute($statementBig);
-    $resultBig = mysqli_stmt_get_result($statementBig);
-
-    if (!$resultBig) {
-        die("Erreur lors de l'exécution de la requête: " . mysqli_error($conn));
-    }
-
-    $count = 0;
-    $violet = 0;
-
-    // AFFICHAGE DES POINTS
-    while ($rowBig = mysqli_fetch_assoc($resultBig)) {
-
-        // CALCUL DE LA DANGEROSITÉ DU VOLCAN AFIN DE CHANGER LA COULEUR DU POINT
-        $lati = $rowBig["latitude"];
-        $longi = $rowBig["longitude"];
-        $danger = "SELECT SUM(population_within_5_km) + SUM(population_within_10_km) AS total_population FROM sae203_volcano WHERE latitude = ? AND longitude = ?";
-        $statementDanger = mysqli_prepare($conn, $danger);
-        mysqli_stmt_bind_param($statementDanger, 'dd', $lati, $longi);
-        mysqli_stmt_execute($statementDanger);
-        $resultDanger = mysqli_stmt_get_result($statementDanger);
-        $rowDanger = mysqli_fetch_assoc($resultDanger);
-        // print_r($rowDanger);
-
-        // VOLCANS DANGEREUX = ROUGE
-        if ($rowDanger['total_population'] >= 100000){
-            $oskour = "RED";
-            echo "<script>addPoint($rowBig[latitude], $rowBig[longitude], 10, Cesium.Color.$oskour);</script>";
-            //$violet += 1;
-
-        // VOLCANS PAS DANGEREUX = ORANGE
-        } else {
-            echo "<script>addPoint($rowBig[latitude], $rowBig[longitude], 10, Cesium.Color.ORANGE);</script>";
-        }
-        //$count += 1;
-    }
-    // echo "Volcans = $count";
-    // echo "Volcans dangereux = $violet";
-    var_dump($rowBig);
-    var_dump($row2);
-    var_dump($oskour);
-}
 ?>
 
-<!-- JAVASCRIPT -->
-
-<!-- PERMET D'AFFICHER EN TEMPS RÉEL LES VALEURS CHOISIES PAR L'UTILISATEUR -->
-
-<script>
-    var eruptionYearRange = document.getElementById("EruptionYear");
-    console.log(eruptionYearRange);
-    var textEruptionYear = document.getElementById("textEruptionYear");
-    console.log(textEruptionYear);
-    
-    eruptionYearRange.addEventListener("input", function () {
-        var indicatorPosition = eruptionYearRange.value;
-        console.log(indicatorPosition);
-
-        // BOUGER LA RANGE = ÉCRIRE DANS LE PLACEHOLDER
-        textEruptionYear.value = indicatorPosition;
-    });
-
-    textEruptionYear.addEventListener("input", function () {
-        var customYear = parseInt(textEruptionYear.value);
-
-        // METTRE MIN ET MAX    
-        if (customYear < -10450) {
-            customYear = -10450;
-        } else if (customYear > 2023) {
-            customYear = 2023;
-        }
-
-        // ÉCRIRE DANS LE PLACEHOLDER = BOUGER LA RANGE 
-        eruptionYearRange.value = customYear;
-    });
-
-    // PAREIL QUE LES LIGNES DU DESSUS
-
-    var elevationIndicator = document.getElementById("elevation");
-    console.log(elevationIndicator);
-    var textElevation = document.getElementById("textElevation");
-    console.log(textElevation);
-
-    elevationIndicator.addEventListener("input", function () {
-        var indicatorPos = elevationIndicator.value;
-        console.log(indicatorPos);
-        textElevation.value = indicatorPos;
-    });
-
-    textElevation.addEventListener("input", function () {
-        var alti = parseInt(textElevation.value);
-
-        // METTRE MIN ET MAX SI L'UTILISATEUR EST BÊTE    
-        if (alti < -10450) {
-            alti = -10450;
-        } else if (alti > 2023) {
-            alti = 2023;
-        }
-
-        // ÉCRIRE DANS LE PLACEHOLDER = BOUGER LA RANGE
-        elevationIndicator.value = alti;
-    });
-</script>
--- formMeteor.php
+++ formMeteor.php
@@ -4,17 +4,89 @@
         <form classe="filtrage" method="post">
             <h4>Filtrer les météorites :</h4>
 
-            <!-- DROPDOWN FOR MAGNITUDE -->
-            <label for="magnitude">Magnitude minimale :</label>
-            <select name="magnitude">
-                <option value="">Tous</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
+            <label for="year">Ancienneté minimale</label>
+            <input id="mYear" type="range" name="mYear" min="860" max="2013">
+            <input name="txtYearM" id="txtYearM" placeholder="Choisir une date minimale"></input>
+            <br><br><br><br>
+
+            <label for="mass">Masse(g) minimale</label>
+            <input id="mass" type="range" name="mass" min="0" max="999.9">
+            <input name="txtMass" id="txtMass" placeholder="Choisir une masse minimale"></input>
+            <br><br><br><br>
+
+            <label for="nombre">Nombre à afficher</label>
+            <select name="totalM">
+                <option value="5000">5000</option>
+                <option value="10000">10000</option>
+                <option value="15000">15000</option>
+                <option value="20000">20000</option>
+                <option value="25000">25000</option>
+                <option value="30000">30000</option>
+                <option value="35000">35000</option>
+                <option value="40000">40000</option>
+                <option value="45716">MAX</option>
             </select>
+            <p> Attention, les grandes valeurs demandent un temps de chargement élevé ! <p>
+            <br><br><br><br>
+
             <!-- BOUTON APPLIQUER LE FILTRE -->
             <input type="submit" name="filterMeteor" value="Appliquer le filtre">
         </form>
     <br>
 </div>
+
+<script>
+var mYearRange = document.getElementById("mYear");
+console.log(mYearRange);
+var txt_mYearRange = document.getElementById("txtYearM");
+console.log(txt_mYearRange);
+
+mYearRange.addEventListener("input", function () {
+    var indicatorPosition = mYearRange.value;
+    console.log(indicatorPosition);
+
+    // BOUGER LA RANGE = ÉCRIRE DANS LE PLACEHOLDER
+    txt_mYearRange.value = indicatorPosition;
+});
+
+txt_mYearRange.addEventListener("input", function () {
+    var customYear = parseInt(txt_mYearRange.value);
+
+    // METTRE MIN ET MAX    
+    if (customYear < -10450) {
+        customYear = -10450;
+    } else if (customYear > 2023) {
+        customYear = 2023;
+    }
+
+    // ÉCRIRE DANS LE PLACEHOLDER = BOUGER LA RANGE 
+    mYearRange.value = customYear;
+});
+
+// PAREIL QUE LES LIGNES DU DESSUS
+
+var massIndicator = document.getElementById("mass");
+console.log(massIndicator);
+var textMass = document.getElementById("txtMass");
+console.log(textMass);
+
+massIndicator.addEventListener("input", function () {
+    var indicatorPos = massIndicator.value;
+    console.log(indicatorPos);
+    textMass.value = indicatorPos;
+});
+
+textMass.addEventListener("input", function () {
+    var lourd = parseInt(textMass.value);
+
+    // METTRE MIN ET MAX SI L'UTILISATEUR EST BÊTE    
+    if (lourd < -10450) {
+        lourd = -10450;
+    } else if (lourd > 2023) {
+        lourd = 2023;
+    }
+
+    // ÉCRIRE DANS LE PLACEHOLDER = BOUGER LA RANGE
+    elevationIndicator.value = lourd;
+});
+</script>
